0ca27d7cd7d55ff8fae30a7da6051ffb56f1d258
mmhorton910 <mishona.horton@tufts.edu>
2023-07-18 14:48:55 -0400

    Added scene 1 script and gamehandler object

--- ShonaGame/Assets/Scene1Dialogue.cs
+++ ShonaGame/Assets/Scene1Dialogue.cs
@@ -11,18 +11,20 @@ public class Scene1Dialogue : MonoBehaviour {
         public Text Char1speech;
         public Text Char2name;
         public Text Char2speech;
-       //public Text Char3name;
-       //public Text Char3speech;
+        public Text Char3name;
+        public Text Char3speech;
         public GameObject DialogueDisplay;
         public GameObject ArtChar1a;
-       //public GameObject ArtChar1b;
-       //public GameObject ArtChar2;
+        //public GameObject ArtChar1b;
+        public GameObject ArtChar2a;
+        public GameObject ArtChar2b;
         public GameObject ArtBG1;
         public GameObject Choice1a;
         public GameObject Choice1b;
         public GameObject NextScene1Button;
         public GameObject NextScene2Button;
         public GameObject nextButton;
+        public GameObject menuButton;
        //public AudioSource audioSource;
         private bool allowSpace = true;
 
@@ -30,12 +32,15 @@ public class Scene1Dialogue : MonoBehaviour {
 void Start(){  
         DialogueDisplay.SetActive(false);
         ArtChar1a.SetActive(false);
+        ArtChar2a.SetActive(false);
+        ArtChar2b.SetActive(false);
         ArtBG1.SetActive(true);
         Choice1a.SetActive(false);
         Choice1b.SetActive(false);
         NextScene1Button.SetActive(false);
         NextScene2Button.SetActive(false);
         nextButton.SetActive(true);
+        menuButton.SetActive(false);
    }
 
 void Update(){         // use spacebar as Next button
@@ -55,53 +60,122 @@ public void Next(){
         else if (primeInt == 2){
                 ArtChar1a.SetActive(true);
                 DialogueDisplay.SetActive(true);
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Wakey wakey, human.";
+                Char1name.text = "YOU";
+                Char1speech.text = "Felix! Felix! Guess what?!";
+                Char2name.text = "";
+                Char2speech.text = "";
         }
        else if (primeInt ==3){
-                Char1name.text = "YOU";
-                Char1speech.text = "Wuh..? What happened?";
-                Char2name.text = "";
-                Char2speech.text = "";
-                //gameHandler.AddPlayerStat(1);
+                ArtChar1a.SetActive(false);
+                ArtChar2a.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Felix";
+                Char2speech.text = "Oh geez, what do you want? I’ve already seen you chase your tail three times today, it’s gotten pretty old.";
         }
        else if (primeInt == 4){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "I know I did not hit you that hard.";
+                ArtChar1a.SetActive(true);
+                ArtChar2a.SetActive(false);
+                Char1name.text = "YOU";
+                Char1speech.text = "No not that! Our guardian and I are going to the dog park!";
+                Char2name.text = "";
+                Char2speech.text = "";
         }
        else if (primeInt == 5){
-                Char1name.text = "YOU";
-                Char1speech.text = "Hit me? Why?";
-                Char2name.text = "";
-                Char2speech.text = "";
+                ArtChar1a.SetActive(false);
+                ArtChar2a.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Felix";
+                Char2speech.text = "And so it begins. Do you think you’ll actually make it to the park this time?";
         }
        else if (primeInt == 6){
-                Char1name.text = "";
-                Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "I am searching for a fugitive. Ragu Fahn.";
-        }
-       else if (primeInt ==7){
+                ArtChar1a.SetActive(true);
+                ArtChar2a.SetActive(false);
                 Char1name.text = "YOU";
-                Char1speech.text = "Why do you think I know anything?";
+                Char1speech.text = "Huh? What are you talking about?";
                 Char2name.text = "";
                 Char2speech.text = "";
         }
-       else if (primeInt == 8){
+       else if (primeInt ==7){
+                ArtChar1a.SetActive(false);
+                ArtChar2a.SetActive(true);
                 Char1name.text = "";
                 Char1speech.text = "";
-                Char2name.text = "Jeda";
-                Char2speech.text = "Do not play the stupid. You will take me to him.";
+                Char2name.text = "Felix";
+                Char2speech.text = "Almost every time you try to go to the park, something happens that freaks our poor guardian out and you come home early.";
+        }
+       else if (primeInt == 8){
+                ArtChar1a.SetActive(true);
+                ArtChar2a.SetActive(false);
+                Char1name.text = "YOU";
+                Char1speech.text = "What? That’s not true!";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+        else if (primeInt == 9){
+                ArtChar1a.SetActive(false);
+                ArtChar2a.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Felix";
+                Char2speech.text = "Oh it’s true, you haven’t been to the park in a week and a half. I know because that was the last time I got any peace in this house.";
+        }
+        else if (primeInt == 10){
+                ArtChar1a.SetActive(true);
+                ArtChar2a.SetActive(false);
+                Char1name.text = "YOU";
+                Char1speech.text = "Oh… I guess you’re right. But today I’m gonna make it to the park! Nothing will get in my way!";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+        else if (primeInt == 11){
+                ArtChar1a.SetActive(false);
+                ArtChar2a.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Felix";
+                Char2speech.text = "Boy I hope so, I could really use the peace and quiet.";
+        }
+        else if (primeInt == 12){
+                ArtChar1a.SetActive(true);
+                ArtChar2a.SetActive(false);
+                Char1name.text = "YOU";
+                Char1speech.text = "Wait do you smell that… it smells like… a used paper towel!! I see it, it’s on the floor in the kitchen!";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+        else if (primeInt == 13){
+                ArtChar1a.SetActive(false);
+                ArtChar2a.SetActive(false);
+                ArtChar2b.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Felix";
+                Char2speech.text = "You’re going to eat it, aren’t you?";
+        }
+        else if (primeInt == 14){
+                ArtChar1a.SetActive(true);
+                ArtChar2b.SetActive(false);
+                Char1name.text = "YOU";
+                Char1speech.text = "… Maybe…";
+                Char2name.text = "";
+                Char2speech.text = "";
+        }
+        else if (primeInt == 15){
+                ArtChar1a.SetActive(false);
+                ArtChar2b.SetActive(true);
+                Char1name.text = "";
+                Char1speech.text = "";
+                Char2name.text = "Felix";
+                Char2speech.text = "I’ll never understand why you do that.";
                 // Turn off "Next" button, turn on "Choice" buttons
                 nextButton.SetActive(false);
                 allowSpace = false;
                 Choice1a.SetActive(true); // function Choice1aFunct()
                 Choice1b.SetActive(true); // function Choice1bFunct()
         }
+        
 
        // after choice 1a
        else if (primeInt == 20){
