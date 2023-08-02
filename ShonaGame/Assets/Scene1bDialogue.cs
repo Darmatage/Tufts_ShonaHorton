0f1e66e1bdde0dd07ea230e6de5307b45c0339b1
mmhorton910 <mishona.horton@tufts.edu>
2023-08-02 13:43:09 -0400

    began editing scence 1b

---
+++ ShonaGame/Assets/Scene1bDialogue.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene1bDialogue : MonoBehaviour
+{
+        public int primeInt = 1;         // This integer drives game progress!
+        public Text Char1name;
+        public Text Char1speech;
+        public Text Char2name;
+        public Text Char2speech;
+        public Text Char3name;
+        public Text Char3speech;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
