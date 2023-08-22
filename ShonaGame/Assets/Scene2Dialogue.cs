16b468e361236dc02ebeeea3e1a9f61ad27cd4af
mmhorton910 <mishona.horton@tufts.edu>
2023-08-22 14:18:09 -0400

    created scene 2

---
+++ ShonaGame/Assets/Scene2Dialogue.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene2Dialogue : MonoBehaviour
+{
+    public int primeInt = 1;         // This integer drives game progress!
+        public Text Char1name;
+        public Text Char1speech;
+        public Text Char2name;
+        public Text Char2speech;
+        public Text Char3name;
+        public Text Char3speech;
+        public GameObject DialogueDisplay;
+        public GameObject ArtChar1a;
+        public GameObject ArtChar1b;
+        public GameObject ArtChar2a;
+        public GameObject ArtChar2b;
+        public GameObject ArtChar3a;
+        public GameObject ArtChar3b;
+        public GameObject ArtBG1;
+        public GameObject Choice1a;
+        public GameObject Choice1b;
+        public GameObject nextButton;
+        public GameObject nextScene1;
+        public GameObject nextScene2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Next()
+    {
+        
+    }
+}
