d4e78526c6eb94edfb4ff3fad4165c0d06cd5b18
mmhorton910 <mishona.horton@tufts.edu>
2023-09-11 20:34:22 -0400

    completed scene 6a script

---
+++ ShonaGame/Assets/Scene6aDialogue.cs
@@ -0,0 +1,226 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class Scene6aDialogue : MonoBehaviour
+{
+    public int primeInt = 1;
+    public Text Char1name;
+    public Text Char1speech;
+    public Text Char2name;
+    public Text Char2speech;
+    public Text Char3name;
+    public Text Char3speech;
+    public GameObject DialogueDisplay;
+    GameObject ArtChar1;
+    GameObject ArtChar1Lose;
+    public GameObject ArtChar1a;
+    public GameObject ArtChar1b;
+    public GameObject ArtChar1c;
+    public GameObject ArtCharLose1a;
+    public GameObject ArtCharLose1b;
+    public GameObject ArtCharLose1c;
+    public GameObject ArtChar2a;
+    public GameObject ArtChar2b;
+    public GameObject ArtChar3a;
+    public GameObject ArtChar3b;
+    public GameObject ArtBG1;
+    public GameObject Choice1a;
+    public GameObject Choice1b;
+    public GameObject Choice2a;
+    public GameObject Choice2b;
+    public GameObject Choice3a;
+    public GameObject Choice3b;
+    public GameObject nextButton;
+    public GameObject nextScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DialogueDisplay.SetActive(false); 
+        ArtChar1a.SetActive(false);
+        ArtChar1b.SetActive(false);
+        ArtChar1c.SetActive(false);
+        ArtCharLose1a.SetActive(false);
+        ArtCharLose1b.SetActive(false);
+        ArtCharLose1c.SetActive(false);
+        ArtChar2a.SetActive(false);
+        ArtChar2b.SetActive(false);
+        ArtChar3a.SetActive(false);
+        ArtChar3b.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        Choice2a.SetActive(false);
+        Choice2b.SetActive(false);
+        Choice3a.SetActive(false);
+        Choice3b.SetActive(false);
+        nextScene.SetActive(false);
+        if (GameHandler.collarColor == 0){
+            if (GameHandler.ateTowel == 0){
+                ArtChar1 = ArtChar1a;
+            }
+            else{
+                ArtChar1 = ArtChar1c;
+            }
+        }
+        else{
+            ArtChar1 = ArtChar1b;
+        }
+        if (GameHandler.collarColor == 0){
+            if (GameHandler.ateTowel == 0){
+                ArtChar1Lose = ArtCharLose1a;
+            }
+            else{
+                ArtChar1 = ArtCharLose1c;
+            }
+        }
+        else{
+            ArtChar1 = ArtCharLose1b;
+        }
+        ArtChar1.SetActive(false);
+        ArtChar1Lose.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    // Update is called once per frame
+    public void Next()
+    {
+        primeInt = primeInt + 1;
+        if (primeInt == 1){
+
+        }
+        else if (primeInt == 2){
+            ArtChar1.SetActive(true);
+            Char1name.text = "YOU";
+            Char1speech.text = "Ooo the lake!! Look at all the little ducks!";
+            Char2name.text = "";
+            Char2speech.text = "";
+            Char3name.text = "";
+            Char3speech.text = "";
+        }
+        else if (primeInt == 3){
+            ArtChar1.SetActive(false);
+            ArtChar3a.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char3name.text = "Guardian";
+            Char3speech.text = "Woah careful, [dog]! Don’t splash me!";
+        }
+        else if (primeInt == 4){
+            ArtChar3a.SetActive(false);
+            ArtChar1.SetActive(true);
+            Char1name.text = "YOU";
+            Char1speech.text = "Throw my toy, throw my toy!!";
+            Char3name.text = "";
+            Char3speech.text = "";
+            if (GameHandler.nice == 1){
+                primeInt = 10;
+            }
+            else{
+                primeInt = 15;
+            }
+        }
+        else if (primeInt == 11){
+            ArtChar2a.SetActive(true);
+            ArtChar1.SetActive(false);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Ollie";
+            Char2speech.text = "Hey [dog]! It’s me, Ollie!";
+        }
+        else if (primeInt == 12){
+            ArtChar1.SetActive(true);
+            ArtChar2a.SetActive(false);
+            Char1name.text = "YOU";
+            Char1speech.text = "Oh my goodness!! Hi Ollie!! Wanna play with my toy with me?";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 13){
+            ArtChar2a.SetActive(true);
+            ArtChar1.SetActive(false);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "Ollie";
+            Char2speech.text = "Yes, let's do it!!";
+            primeInt = 20;
+        }
+        else if (primeInt == 16){
+            ArtChar3a.SetActive(true);
+            ArtChar1.SetActive(false);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char3name.text = "Guardian";
+            Char3speech.text = "Ok [dog], go get it!";
+        }
+        else if (primeInt == 17){
+            ArtChar1.SetActive(true);
+            ArtChar3a.SetActive(false);
+            Char1name.text = "YOU";
+            Char1speech.text = "I got it, I got it!!";
+            Char3name.text = "";
+            Char3speech.text = "";
+        }
+        else if (primeInt == 18){
+            ArtChar3a.SetActive(true);
+            ArtChar1.SetActive(false);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char3name.text = "Guardian";
+            Char3speech.text = "Good job buddy!";
+            primeInt = 20;
+        }
+        else if (primeInt == 21){
+            ArtChar2a.SetActive(false);
+            ArtChar3a.SetActive(false);
+            ArtChar1.SetActive(true);
+            Char1name.text = "YOU";
+            Char1speech.text = "This is so fun!!";
+            Char2name.text = "";
+            Char2speech.text = "";
+            Char3name.text = "";
+            Char3speech.text = "";
+        }
+        else if (primeInt == 22){
+            ArtChar1.SetActive(false);
+            ArtChar3a.SetActive(true);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char3name.text = "Guardian";
+            Char3speech.text = "You really love the park, don’t you [dog]?";
+        }
+        else if (primeInt == 23){
+            ArtChar3a.SetActive(false);
+            ArtChar1.SetActive(true);
+            Char1name.text = "YOU";
+            Char1speech.text = "Yes yes yes! This is the best day ever!!";
+            Char3name.text = "";
+            Char3speech.text = "";
+        }
+        else if (primeInt == 24){
+            ArtChar3a.SetActive(true);
+            ArtChar1.SetActive(false);
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char3name.text = "Guardian";
+            Char3speech.text = "I’m glad you’re having a good time, buddy. Come on, let's go play with the ducks some more!";
+        }
+        else if (primeInt == 25){
+            ArtChar3a.SetActive(false);
+            ArtChar1.SetActive(true);
+            Char1name.text = "YOU";
+            Char1speech.text = "Ok!!";
+            Char3name.text = "";
+            Char3speech.text = "";
+
+            nextButton.SetActive(false);
+            nextScene.SetActive(true);
+        }
+    }
+    public void SceneChange(){
+        SceneManager.LoadScene("SceneWin");
+    }
+}
